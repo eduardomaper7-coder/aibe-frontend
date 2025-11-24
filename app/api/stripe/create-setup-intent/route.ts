adcf6d6c0a24bd06cc981cfbe1980fc61aecb821
eduardomaper7-coder <eduardomaper7@gmail.com>
2025-11-24 17:58:39 +0100

    mejorar-movil

--- app/api/stripe/create-setup-intent/route.ts
+++ app/api/stripe/create-setup-intent/route.ts
@@ -26,7 +26,8 @@ export async function POST(req: NextRequest) {
     // 3) OBTENER O CREAR STRIPE CUSTOMER
     // -------------------------------------------------------------
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2023-10-16",
+      apiVersion: "2025-09-30.clover"
+,
     });
 
     let customerId =
--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -9,7 +9,7 @@ export const config = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2025-09-30.clover",
 });
 const supabaseAdmin = getSupabaseAdmin();
 
