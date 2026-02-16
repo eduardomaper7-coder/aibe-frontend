c18d4173e3ccc05c6e88e4d4eef8aa3c3d616ae1
eduardomaper7-coder <eduardomaper7@gmail.com>
2026-02-16 13:50:49 +0100

    mejora

--- app/[locale]/panel/layout.tsx
+++ app/[locale]/panel/layout.tsx
@@ -1,14 +1,10 @@
-// app/[locale]/panel/layout.tsx
 import PanelHeader from "./PanelHeader";
-import PanelShell from "./PanelShell";
 
 export default function PanelLayout({ children }: { children: React.ReactNode }) {
   return (
-    <PanelShell>
-      <div className="min-h-screen bg-white text-black">
-        <PanelHeader />
-        {children}
-      </div>
-    </PanelShell>
+    <div className="min-h-screen bg-white text-black">
+      <PanelHeader />
+      {children}
+    </div>
   );
 }
