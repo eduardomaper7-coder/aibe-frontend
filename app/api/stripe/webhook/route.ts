3e0329ad125cb297c8db64c8f5398faaf1554250
eduardomaper7-coder <eduardomaper7@gmail.com>
2026-02-17 11:41:12 +0100

    mejora

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -4,9 +4,8 @@ import { NextResponse } from "next/server";
 // IMPORTANTE: en webhooks necesitamos el body "raw"
 export const runtime = "nodejs";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-06-20",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "");
+
 
 export async function POST(req: Request) {
   const sig = req.headers.get("stripe-signature");
