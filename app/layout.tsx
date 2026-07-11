effe9a6878c25c76d65cce190cf53ee63e75238c
Jorgitoo <58446673+Jorgitoo@users.noreply.github.com>
2026-07-11 16:18:46 +0000

    arreglar css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
-// app/layout.tsx
 import "./globals.css";
 import "leaflet/dist/leaflet.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "700"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
+    <html lang="es" className={inter.className}>
       <body>{children}</body>
     </html>
   );
