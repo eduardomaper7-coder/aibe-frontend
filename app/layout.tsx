987805cf5b93186a066e35d02ed97cf8cc9c0e86
eduardomaper7-coder <eduardomaper7@gmail.com>
2026-07-18 20:54:15 +0100

    mejora

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Loader from "@/components/Loader";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -12,6 +13,21 @@ export default function RootLayout({
         <Loader>
           {children}
         </Loader>
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18263984994"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'AW-18263984994');
+          `}
+        </Script>
       </body>
     </html>
   );
